5b6ba9ad3309d5eee01055997d04a4d564bf3a69
simi.jain <simi.jain@acsicorp.com>
2024-06-10 16:34:59 +0530

    inital commit

---
+++ health.py
@@ -0,0 +1,62 @@
+import json
+import requests
+import variables
+
+def lambda_handler(event, context):
+    
+    print("event--->",event)
+    
+    # Access the 'service' key within the object
+    event_arn = event['eventArn']
+    service = event['service']
+    event_type = event['eventTypeCode']
+    description = event['eventDescription'][0]['latestDescription']
+ 
+ 
+    print("summary:",service,"description:",description)
+    
+    # Create Jira issue payload
+    jira_issue_data = {
+        "fields": {
+            "project": {
+                "key": variables.PROJECT_KEY
+            },
+            "summary": f"{event_type}, Service: {service} ",
+            "description": {
+                "content": [
+                    {
+                        "content": [
+                            {
+                                "text": f" AWS Service Health details: \n {description}.",
+                                "type": "text"
+                                }
+                            ],
+                            "type": "paragraph"
+                        }
+                    ],
+                    "type": "doc",
+                    "version": 1
+                
+            },
+            "issuetype": {
+                "name": "Task"
+            }
+            # Add other fields as needed
+        }
+    }
+    
+    
+    # Make a POST request to the Jira API
+    jira_url = variables.JIRA_URL
+    user_name = variables.USER_NAME
+    API_Token = variables.API_TOKEN
+    jira_auth = (user_name, API_Token)
+    response = requests.post(jira_url, json=jira_issue_data, auth=jira_auth)
+    print("response:",response)
+    
+    if response.status_code == 201:
+        print("Jira issue created successfully")
+        issue_key = response.json()["key"]
+        print(issue_key)
+    else:
+        print("Failed to create Jira issue:", response.text)
